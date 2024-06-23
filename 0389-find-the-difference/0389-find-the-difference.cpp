835ef977d494794721492d35d0f3f24a0661ef33
Gautam Jangir <83230112+Gautam-Jangir8@users.noreply.github.com>
2024-06-23 22:25:54 +0530

    Time: 4 ms (49.09%), Space: 7.8 MB (83.9%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int ans = 0;
+
+        for(int i=0; i<s.length(); i++)
+        {
+            ans ^= s[i]-'a';
+        }
+        for(int i=0; i<t.length(); i++)
+        {
+            ans ^= t[i]-'a';
+        }
+
+        return (char)(ans+'a');
+    }
+};
