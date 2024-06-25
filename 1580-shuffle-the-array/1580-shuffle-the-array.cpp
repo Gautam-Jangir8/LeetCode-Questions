513f1068f67906a5b295eb74ac2f08f654d7749a
Gautam Jangir <83230112+Gautam-Jangir8@users.noreply.github.com>
2024-06-25 23:39:59 +0530

    Time: 4 ms (62.22%), Space: 12 MB (73.85%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.cpp
+++ 1580-shuffle-the-array/1580-shuffle-the-array.cpp
@@ -1,13 +1,37 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int>ans;
-        for(int i=0; i<n; i++)
+        // In O(1) space complexity 
+        int m = 1e3+1;// maximum value of the element +1
+        int e=0, o=n;
+        for(int i=0; i<2*n; i++)
         {
-            ans.push_back(nums[i]);
-            ans.push_back(nums[i+n]);
+            if(i%2==0)
+            {
+                nums[i] += m*(nums[e++]%m);
+            }
+            else
+            {
+                nums[i] += m*(nums[o++]%m);
+            }
         }
 
-        return ans;
+        for(int i=0; i<2*n; i++)
+        {
+            nums[i] = nums[i]/m;
+        }
+
+        return nums;
     }
 };
+
+// vector<int> shuffle(vector<int>& nums, int n) {
+//         int m=1e3+1;
+//         int e=0,o=n;
+//         for(int i=0;i<2*n;i++){
+//             if(i%2==0) nums[i]+=m*(nums[e++]%m);
+//             else nums[i]+=m*(nums[o++]%m);
+//         }
+//         for(int i=0;i<2*n;i++) nums[i]=nums[i]/m;
+//         return nums;   
+//     }
