151072abf75d416257708eb980175ab7f160cd39
Gautam Jangir <83230112+Gautam-Jangir8@users.noreply.github.com>
2024-06-26 08:39:07 +0530

    Time: 0 ms (100%), Space: 7 MB (66.33%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        while(b!=0)
-        {
-            // carry now contains set f set bits. carry contributes to the next bit so it will be left shifted later
-            int carry = a&b;
-
-            // sum of bits of a and b where atleast one of the bits is not set
-            a = a^b;
-
-            // Carry is shifted by one so that adding it to a gives the required sum
-            b = carry<<1;
-        }
-
-        return a;
+         //code here
+        int val1=a^b;
+        int val2=(a&b)<<1;
+        int value=val1+val2 ;
+        return value;
     }
 };
