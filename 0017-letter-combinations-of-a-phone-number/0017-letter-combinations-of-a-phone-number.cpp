09b0eab874654ab107838cd1bd49a465baf4d42c
Gautam Jangir <83230112+Gautam-Jangir8@users.noreply.github.com>
2024-08-21 17:43:57 +0530

    Time: 0 ms (100%), Space: 8.5 MB (35.71%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    void recursion(int idx, string digits, int n, vector<pair<int, string>>vec, vector<string>&ans, string temp)
-    {
-        if(idx==n)
-        {
+    void recursion(int idx, int n, string &digits, vector<string>&ans, vector<pair<int, string>>&vec, string temp) {
+        if(idx==n) {
             ans.push_back(temp);
             return;
         }
-        
+
         int digit = digits[idx]-'0';
-        string s = vec[digit-2].second;
-        for(int i=0; i<s.length(); i++)
-        {
-            temp.push_back(s[i]);
-            recursion(idx+1, digits, n, vec, ans, temp);
+        string str = vec[digit-2].second;
+        for(int i=0; i<str.length(); i++) {
+            temp.push_back(str[i]);
+            recursion(idx+1, n, digits, ans, vec, temp);
             temp.pop_back();
         }
     }
 
     vector<string> letterCombinations(string digits) {
-        if(digits.length()==0)
-        {
+        int n = digits.size();
+        // If the number of digits are zero 
+        if(n==0) {
             return {};
         }
 
-        int N = digits.length();
-        vector<pair<int, string>>vec = {{2, "abc"}, {3, "def"}, {4, "ghi"}, {5, "jkl"},
-            {6, "mno"}, {7, "pqrs"}, {8, "tuv"}, {9, "wxyz"}};
-        vector<string>ans;
-        recursion(0, digits, N, vec, ans, "");
+        vector<pair<int, string>>vec = {{2, "abc"}, {3, "def"}, {4, "ghi"}, {5, "jkl"}, {6, "mno"}, {7, "pqrs"}, {8, "tuv"}, {9, "wxyz"}};
+
+        vector<string>ans; 
+        recursion(0, n, digits, ans, vec, "");
 
         return ans;
     }
