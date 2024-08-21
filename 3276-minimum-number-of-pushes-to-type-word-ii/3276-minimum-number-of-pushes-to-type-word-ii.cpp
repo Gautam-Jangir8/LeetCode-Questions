674c2163242b77d6e825c2f21370d369478107d0
Gautam Jangir <83230112+Gautam-Jangir8@users.noreply.github.com>
2024-08-21 18:08:37 +0530

    Time: 77 ms (42.57%), Space: 25.1 MB (68.06%) - LeetHub

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int>vec(26, 0);
+
+        // All the elements frequency is stored in the vector
+        for(int i=0; i<word.size(); i++) {
+            vec[word[i]-'a']++;
+        }
+
+        int ans = 0;
+        sort(vec.begin(), vec.end(), greater<int>());
+        int cnt = 1;
+        int temp = 1;
+        for(auto i: vec) {
+            if(cnt>8) {
+                temp++;
+                cnt = 1;
+            }
+            if(i>0) {
+                cnt++;
+                ans+=(temp*i);
+            }
+        }
+
+        return ans;
+    }
+};
