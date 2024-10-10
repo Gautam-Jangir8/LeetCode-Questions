84352e04b931435f5b3618fc8560145e7f789d11
Gautam Jangir <83230112+Gautam-Jangir8@users.noreply.github.com>
2024-10-10 11:04:30 +0530

    Time: 13 ms (32.25%), Space: 12.1 MB (15.29%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    int dp[1005][1005];
     int recursion(int n, int cnt, int c) {
         if(cnt>n) {
             return 1e7;
@@ -7,6 +8,8 @@ public:
         if(cnt==n) {
             return 0; // No further steps is required we have reached our destination
         }
+
+        if(dp[cnt][c]!=-1)return dp[cnt][c];
 
         int ans = 1e7;
         if(n%cnt==0) {
@@ -16,10 +19,12 @@ public:
         ans = min(ans, recursion(n, cnt*2, cnt)+2);// update cnt(Pasting), as well as last copied(Copied present char)
         ans = min(ans, recursion(n, cnt+c, c)+1);// Only updated count by (Pasting) last copied i.e. c 
 
-        return ans;
+        return dp[cnt][c] = ans;
     }
 
     int minSteps(int n) {
+        memset(dp, -1, sizeof(dp));
+
         // One size is already present, so retun 0 steps
         if(n==1) {
             return 0;
