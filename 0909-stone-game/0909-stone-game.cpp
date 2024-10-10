a3d8e0651b27e7cdf70a0dd93382343653b9893f
Gautam Jangir <83230112+Gautam-Jangir8@users.noreply.github.com>
2024-10-10 11:59:35 +0530

    Time: 29 ms (7.43%), Space: 76.3 MB (5.54%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
+    int AliceWin(bool turn, int l, int r, vector<int>&piles, vector<vector<int>>&dp) {
+        if(l>r) {
+            return 0; // Now no one can pick, recursion over 
+        }
+        if(l==r) {
+            return piles[l]; // Last step to consider
+        }
+
+        if(dp[l][r]!=-1) {
+            return dp[l][r];
+        }
+
+        int total = 0;
+        if(turn) { // If turn is true, means it's alice turn
+            total += max(piles[l] + AliceWin(!turn, l+1, r, piles, dp), piles[r] + AliceWin(!turn, l, r-1, piles, dp));
+        } else { // Bob's turn
+            total += min(-piles[l] + AliceWin(!turn, l+1, r, piles, dp), -piles[r] + AliceWin(!turn, l, r-1, piles, dp));
+        }
+
+        return dp[l][r] = total;
+    }
+
     bool stoneGame(vector<int>& piles) {
-        return 1;
+        int n = piles.size();
+        vector<vector<int>>dp(505, vector<int>(505, -1));
+
+        // If End score is greater then 0 then Alice wins, return true, else return false
+        return AliceWin(1, 0, n-1, piles, dp) > 0; // Here initial 1 indicates that it is the turn of alice first
     }
 };
