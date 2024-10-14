11036d2ec5502ced91fb35b40c485cb2531d85d5
Gautam Jangir <83230112+Gautam-Jangir8@users.noreply.github.com>
2024-10-14 12:22:42 +0530

    Time: 63 ms (73.21%), Space: 42.2 MB (84.22%) - LeetHub

---
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int>ans = arr;
+        sort(ans.begin(), ans.end());
+        unordered_map<int, int>mp;
+
+        int cnt = 1;
+        for(int i=0; i<ans.size(); i++) {
+            if(mp.find(ans[i])==mp.end()) {
+                mp[ans[i]] = cnt;
+                cnt++;
+            }
+        }
+
+        for(int i=0; i<arr.size(); i++) {
+            ans[i] = mp[arr[i]];
+        }
+
+        return ans;
+    }
+};
