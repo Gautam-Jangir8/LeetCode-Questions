19e0c358f2d6fc3d668000ba4ec11b702ba92194
Gautam Jangir <83230112+Gautam-Jangir8@users.noreply.github.com>
2024-10-15 16:04:28 +0530

    Time: 2 ms (44.42%), Space: 10.7 MB (35.85%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -2,19 +2,21 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int n = nums.size();
-        int cnt = 0;
+        if(n==0) {
+            return {};
+        }
 
-        for (int i = 0; i < n; i++)
-        {
-            if (nums[i] == val)
-            {
-                nums[i] = INT_MAX;
-                cnt++;
+        int i=0, j=n-1;
+
+        while(i<=j) {
+            if(nums[i]==val) {
+                swap(nums[i], nums[j]);
+                j--;
+            } else {
+                i++;
             }
         }
 
-        sort(nums.begin(), nums.end());
-
-        return n-cnt;
+        return i;
     }
 };
