e8f4bd2dde4921c5114b10a169fd91441b8b14ba
Gautam Jangir <83230112+Gautam-Jangir8@users.noreply.github.com>
2024-10-15 17:31:06 +0530

    Time: 23 ms (22.8%), Space: 28.1 MB (14.52%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -5,12 +5,8 @@ public:
         unordered_map<int, int>mp;
         for(auto i: nums) {
             mp[i]++;
-        }
-
-        int count = 0;
-        for(auto i: mp) {
-            if(i.second>(n/2)) {
-                return i.first;
+            if(mp[i]>(n/2)) {
+                return i;
             }
         }
 
