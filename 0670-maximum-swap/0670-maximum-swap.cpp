93d39bcac82d4ba1e6e5bb50be0fd600628597c9
Gautam Jangir <83230112+Gautam-Jangir8@users.noreply.github.com>
2024-10-17 10:13:29 +0530

    Time: 0 ms (100%), Space: 7.6 MB (31.77%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
     int maximumSwap(int num) {
-        int res = num;
-        vector<int>number;
-        while(num>0) {
-            number.push_back(num%10);
-            num = num/10;
+        // Converting number to string for easier digit manipulation
+        string numStr = to_string(num);
+        int n = numStr.size();
+
+        // Store the last occurance of each digit (0-9)
+        vector<int>last(10, -1);
+        for(int i=0; i<n; i++) {
+            last[numStr[i]-'0'] = i;
         }
 
-        reverse(number.begin(), number.end());
-
-        for(int i=0; i<number.size()-1; i++) {
-            int maxiRight = number[i];
-            int maxiIndex = -1;
-            for(int j=i+1; j<number.size(); j++) {
-                if(number[j]>=maxiRight && number[i]!=number[j]) { // So that we can swap last largest
-                    maxiRight = number[j];
-                    maxiIndex = j;
+        // Traverse each digit from left to right
+        for(int i=0; i<n; i++) {
+            // Check if any larger digit appearing later
+            for(int d=9; d>numStr[i]-'0'; d--) {
+                if(last[d]>i) {
+                    // Swap the current digit with the larger one
+                    swap(numStr[i], numStr[last[d]]);
+                    return stoi(numStr);// Convert back to number and return 
                 }
             }
-            if(maxiIndex!=-1) {
-                swap(number[i], number[maxiIndex]);
-                break;
-            }
-        }
+        } 
 
-        int ans = 0;
-        for(int i=0; i<number.size(); i++) {
-            ans = ans*10 + number[i];
-        }
-
-        return max(ans, res);
+        // If no Swap was made return the original number
+        return num;
     }
 };
