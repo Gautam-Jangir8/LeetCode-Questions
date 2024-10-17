e8cdc64e2ad1bc04bc0b36bc93cd1df89bf281e2
Gautam Jangir <83230112+Gautam-Jangir8@users.noreply.github.com>
2024-10-17 15:58:14 +0530

    Time: 7 ms (99.47%), Space: 23.4 MB (71.9%) - LeetHub

--- 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.cpp
+++ 2042-maximum-product-difference-between-two-pairs/2042-maximum-product-difference-between-two-pairs.cpp
@@ -1,9 +1,34 @@
 class Solution {
 public:
-    int maxProductDifference(vector<int>& nums) {
+    int maxProductDifference(vector<int> &nums)
+    {
         int n = nums.size();
-        sort(nums.begin(), nums.end());
+        priority_queue<int> maxpq;
+        priority_queue<int, vector<int>, greater<int>> minpq;
+        maxpq.push(nums[0]);
+        maxpq.push(nums[1]);
+        minpq.push(nums[0]);
+        minpq.push(nums[1]);
 
-        return (nums[n-1]*nums[n-2]) - (nums[0]*nums[1]);
+        for (int i = 2; i < n; i++)
+        {
+            int curr = nums[i];
+            if (curr > minpq.top())
+            {
+                minpq.pop();
+                minpq.push(curr);
+            }
+            if (curr < maxpq.top())
+            {
+                maxpq.pop();
+                maxpq.push(curr);
+            }
+        }
+
+        int x = maxpq.top(), a = minpq.top();
+        maxpq.pop();
+        minpq.pop();
+        int y = maxpq.top(), b = minpq.top();
+        return (a * b) - (x * y);
     }
 };
