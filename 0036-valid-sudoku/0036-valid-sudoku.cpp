8b80cad67de92076d743d23514128e9181420917
Gautam Jangir <83230112+Gautam-Jangir8@users.noreply.github.com>
2024-10-21 12:19:37 +0530

    Time: 8 ms (93.91%), Space: 24.9 MB (39.9%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,18 +1,90 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-         int row[9][9] = {0}, col[9][9] = {0}, grid_3x3[9][9] = {0}; 
-        
-        for(int i = 0; i < board.size(); i++)
-            for(int j = 0; j < board[i].size(); j++)
-                if(board[i][j] != '.')
+   bool validBox(vector<vector<char>> &board, int sr, int er, int sc, int ec)
+    {
+        unordered_set<char> set;
+        for (int i = sr; i <= er; i++)
+        {
+            for (int j = sc; j <= ec; j++)
+            {
+                if (board[i][j] == '.')
                 {
-                    int num = board[i][j] - '1', k = i/3*3+j/3; //k for finding grid index from 0 to 8
-                    if(row[i][num] || col[j][num] || grid_3x3[k][num])
-                        return false;
-                    row[i][num] = col[j][num] = grid_3x3[k][num] = 1;
+                    continue;
                 }
-        
+                if (set.find(board[i][j]) != set.end())
+                {
+                    return false;
+                }
+
+                set.insert(board[i][j]);
+            }
+        }
+
+        return true;
+    }
+
+    bool isValidSudoku(vector<vector<char>> &board)
+    {
+        int n = board.size();
+        // Validate Rows
+        for (int row = 0; row < 9; row++)
+        {
+            unordered_set<char> set;
+            for (int col = 0; col < 9; col++)
+            {
+                if (board[row][col] == '.')
+                {
+                    continue;
+                }
+                if (set.find(board[row][col]) != set.end())
+                {
+                    return false; // repeated value
+                }
+                else
+                {
+                    set.insert(board[row][col]);
+                }
+            }
+        }
+
+        // Validate Columns
+        for (int col = 0; col < 9; col++)
+        {
+            unordered_set<char> set;
+            for (int row = 0; row < 9; row++)
+            {
+                if (board[row][col] == '.')
+                {
+                    continue;
+                }
+                if (set.find(board[row][col]) != set.end())
+                {
+                    return false; // repeated value
+                }
+                else
+                {
+                    set.insert(board[row][col]);
+                }
+            }
+        }
+
+        // validate Grids(3*3)
+        for (int sr = 0; sr < 9; sr += 3)
+        {
+            int er = sr + 2;
+
+            for (int sc = 0; sc < 9; sc += 3)
+            {
+                int ec = sc + 2;
+
+                // sr, er, sc, ec
+                if (!validBox(board, sr, er, sc, ec))
+                {
+                    return false;
+                }
+            }
+        }
+
         return true;
     }
 };
