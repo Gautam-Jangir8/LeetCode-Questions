61245ea7e7c86bd19008edd0e0e58ca9a26bbe54
Gautam Jangir <83230112+Gautam-Jangir8@users.noreply.github.com>
2025-01-02 07:32:05 +0530

    Time: 4 ms (81.49%), Space: 70.1 MB (53.91%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cpp
@@ -1,40 +1,21 @@
 class Solution {
 public:
-    bool isVowelStartEnd(string str) {
-        int end = str.size()-1;
-        if((str[0]=='a' || str[0]=='e' || str[0]=='i' || str[0]=='o' || str[0]=='u') && (str[end]=='a' || str[end]=='e' || str[end]=='i' || str[end]=='o' || str[end]=='u')) {
-            return true;
-        }
-        return false;
-    }
-
     vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
-        vector<int>ans;
-        unordered_map<int, int>mp;
-        int cnt = 0;
-        for(int i=0; i<words.size(); i++) {
-            string single = words[i];
-            if(isVowelStartEnd(single)) {
-                cnt++;
+        int n = words.size();
+        vector<int> Prefix(n + 1, 0);
+        unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u'}; 
+        for (int i = 0; i < n; i++) {
+            Prefix[i + 1] = Prefix[i];
+            if (vowels.count(words[i].front()) && vowels.count(words[i].back())) {
+                Prefix[i + 1]++;  
             }
-            mp[i] = cnt;
-            cout<<mp[i]<<" ";
+        }
+        vector<int> ANS; 
+        for (auto& query : queries) {
+            int L = query[0], R = query[1];  
+            ANS.push_back(Prefix[R + 1] - Prefix[L]);  
         }
 
-    
-
-        for(int i=0; i<queries.size(); i++) {
-            int start = queries[i][0];
-            int end = queries[i][1];
-
-            // if(start==end) {
-            //     ans.push_back(0);
-            // } else {
-            // }
-                int temp = start==0 ?  0 : mp[start-1];
-                ans.push_back(mp[end]-temp);
-        }
-
-        return ans;
+        return ANS;  
     }
 };
