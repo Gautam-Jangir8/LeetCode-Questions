79a1de784cc4e3b022a72979af413e039fbff324
Gautam Jangir <83230112+Gautam-Jangir8@users.noreply.github.com>
2025-01-08 09:30:59 +0530

    Time: 20 ms (6.88%), Space: 16.2 MB (8.29%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
@@ -1,24 +1,67 @@
 class Solution {
 public:
-    vector<string> stringMatching(vector<string>& words) {
-        int n = words.size();
-        sort(words.begin(), words.end(), 
-             [&](const string &a, const string &b) -> bool {
-                 return a.size() > b.size();
-             });
-        vector<string>ans;
+    static bool comp(string &a, string &b) {
+        return a.size()>b.size();
+    }
 
-        for(int i=0; i<n; i++) {
-            for(int j=i+1; j<n; j++) {
-                if(words[i].find(words[j])!=-1) {
-                    ans.push_back(words[j]);
+    vector<int> findLPS(string word) {
+        int n = word.size();
+        vector<int>lps(n, 0);
+
+        int pre = 0, suf = 1;
+        while(suf<n) {
+            if(word[pre]==word[suf]) {
+                lps[suf] = pre+1;
+                pre++;
+                suf++;
+            } else { // There will be two case this time
+                if(pre==0) {
+                    suf++;
+                } else {
+                    pre = lps[pre-1];
                 }
             }
         }
 
-        sort(ans.begin(), ans.end());
-        ans.erase(unique(ans.begin(), ans.end()), ans.end());
+        return lps;
+    }
 
-        return ans;
+    vector<string> findMatchingString(vector<string>& words) {
+        vector<string>ans;
+        int n = words.size();
+
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+                vector<int>lps = findLPS(words[j]);
+
+                int p=0, q=0;
+                while(p<words[i].size()) {
+                    if(words[i][p]==words[j][q]) {
+                        p++;
+                        q++;
+                    }
+                    if(q==words[j].size()) {
+                        ans.push_back(words[j]);
+                        break;
+                    } else if(p<words[i].size() && words[i][p]!=words[j][q]) {
+                        if(q==0) {
+                            p++;
+                        } else {
+                            q = lps[q-1];
+                        }
+                    }
+                }
+            }
+        }
+
+         // Remove duplicates using a set
+        set<string> uniqueWords(ans.begin(), ans.end());
+        return vector<string>(uniqueWords.begin(), uniqueWords.end());
+    }
+
+    vector<string> stringMatching(vector<string>& words) {
+        sort(words.begin(), words.end(), comp);
+
+        return findMatchingString(words);
     }
 };
