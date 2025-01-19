7c81ade54204f2312d843a8b068e19bd70d9c662
Gautam Jangir <83230112+Gautam-Jangir8@users.noreply.github.com>
2025-01-19 16:31:33 +0530

    Time: 27 ms (29.02%), Space: 187.5 MB (21.65%) - LeetHub

--- 1568-pseudo-palindromic-paths-in-a-binary-tree/1568-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1568-pseudo-palindromic-paths-in-a-binary-tree/1568-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -13,29 +13,27 @@ class Solution {
 public:
     int pseudoPalindromicPaths (TreeNode* root) {
         int cnt = 0;
-
         stack<pair<TreeNode*, int>>stk;
         stk.push({root, 0});
 
         while(!stk.empty()) {
             pair<TreeNode*, int>p = stk.top();
+            stk.pop();
             TreeNode* node = p.first;
             int path = p.second;
-            stk.pop();
 
             if(node!=NULL) {
-                path = path ^ (1<<node->val);
-
-                if(node->left==NULL && node->right==NULL) {
+                path = path^(1<<node->val);
+                if(node->left==NULL && node->right==NULL) { // Its a leaf node
                     if((path & (path-1))==0) {
                         cnt++;
                     }
                 } else {
-                    stk.push({node->left, path});
                     stk.push({node->right, path});
+                    stk.push({node->left, path});
                 }
             }
-        }
+        }   
 
         return cnt;
     }
