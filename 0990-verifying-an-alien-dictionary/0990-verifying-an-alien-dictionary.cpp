7eb57c8c3b24207e4b5608554f69c79ee804f23b
Gautam Jangir <83230112+Gautam-Jangir8@users.noreply.github.com>
2025-02-06 10:00:59 +0530

    Time: 5 ms (4.84%), Space: 14.4 MB (5.03%) - LeetHub

--- 0990-verifying-an-alien-dictionary/0990-verifying-an-alien-dictionary.cpp
+++ 0990-verifying-an-alien-dictionary/0990-verifying-an-alien-dictionary.cpp
@@ -1,45 +1,27 @@
 class Solution {
 public:
-
     bool isAlienSorted(vector<string>& words, string order) {
-        // If there is only one word to check then, its not valid, minimum two words are required to finnd the order
-        if (words.size() == 1)
-        {
-            return true;
-        }
+        unordered_map<char, int>mp;
 
-        // character and their rank is stored in the map
-        unordered_map<char, int> mp;
-        for (int i = 0; i < order.size(); i++)
-        {
+        for(int i=0; i<order.size(); i++) {
             mp[order[i]] = i;
         }
 
-        // Traversing the array
-        for (int i = 0; i < words.size() - 1; i++)
-        {
-            // Traverse each character in a word
-            for (int j = 0; j < words[i].size(); j++)
-            {
-                // If all the words has matched so far and current word length is longer then the next word, then return false
-                if (j >= words[i + 1].size())
-                {
-                    return false;
-                }
+        vector<vector<string>>ans;
+        for(int i=0; i<words.size(); i++) {
+            string str = words[i];
 
-                // Checking if letters in the same position in two words are different
-                if (words[i][j] != words[i + 1][j])
-                {
-                    // Checkif the rank of the word in teh curren t word is greater then the same same charcter in the other word
-                    if (mp[words[i][j]] > mp[words[i + 1][j]])
-                    {
-                        return false;
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
+            for(int j=0; j<str.size(); j++) {
+                str[j] = 'a'+mp[str[j]];
+            }
+            ans.push_back({str, words[i]});
+        }
+
+        sort(ans.begin(), ans.end());
+
+        for(int i=0; i<ans.size(); i++) {
+            if(words[i]!=ans[i][1]) {
+                return false;
             }
         }
 
